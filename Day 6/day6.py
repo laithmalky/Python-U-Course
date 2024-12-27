e485abb1ac6f8ac9b661ba8a6450888b12b5c8ea
Laith Malky <laith.malky@gmail.com>
2024-12-27 23:36:32 +0200

    commit

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -2,6 +2,11 @@
 # len_chr = len("Hello")
 # print(len_chr)
 
+# the print is function and len is function and any thing ends with () is function and etc...
+
+
+# create first function
+
 def first_function():
     print("How r u?")
     print("Bye bye")
