1c146ca06c9105757b7186a39472e3d02605f4e3
Laith Malky <laith.malky@gmail.com>
2024-12-28 23:00:04 +0200

    commit

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -20,3 +20,41 @@ def hello():
   print("hi")
 
 hello()
+
+
+# robot task 
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
+'''
+
+def turn_around():
+    turn_left()
+    turn_left()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def finish_wall():
+    turn_right()
+    move()
+    turn_right()
+    
+def jump_robo():
+    turn_left()
+    while wall_on_right():
+        move()
+    finish_wall()
+    while front_is_clear():
+        move()
+    turn_left()
+
+while at_goal() == False:
+    if wall_in_front():
+        jump_robo()
+    else:
+        move()
+
+'''
