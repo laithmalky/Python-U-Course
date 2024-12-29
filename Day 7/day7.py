cfd79c8216e26d4d70e026ea6caad220ed11d708
Laith Malky <laith.malky@gmail.com>
2024-12-29 21:41:34 +0200

    commit

--- Day 7/day7.py
+++ Day 7/day7.py
@@ -2,14 +2,25 @@ import random
 
 words_list = ["Apple", "Welcome", "Hello"]
 
-word = words_list[random.randint(0, len(words_list))]
+# word = words_list[random.randint(0, len(words_list))]
+word = random.choice(words_list)
+
 
 print(word)
 
+under_score = ""
+for blanks in range(0,len(word)):
+    under_score += "_"
+print(under_score)
+
 user_input = input("Please write a letter: ").lower()
 
+guesses = ""
 for letter in word:
     if user_input == letter.lower():
-        print("Right")
+        guesses += letter
+        #print("Right")
     else:
-        print("Wrong")
+        guesses += "_"
+        #print("Wrong")
+print(guesses)
