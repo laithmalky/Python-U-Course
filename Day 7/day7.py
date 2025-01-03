5802f2b5b00fd13508ad6c361f17d2f0ef11166e
Laith Malky <laith.malky@gmail.com>
2025-01-03 15:11:39 +0200

    commit

--- Day 7/day7.py
+++ Day 7/day7.py
@@ -75,7 +75,7 @@ print("Welcome to the hangman game")
 
 word = random.choice(words_list.words_listing)
 
-#print(word)
+print(word)
 
 under_score = ""
 for blanks in range(len(word)):
@@ -87,22 +87,30 @@ correct_word_list = []
 correct_word = ""
 
 lives = 6
+used_before = False
 
 while game_over != False:
 
     guesses = ""
     user_input = input("Please write a letter: ").lower()
+    if user_input in correct_word:
+        used_before = True
+        lives -= 1
+
     for letter in word:
         if user_input == letter:
             guesses += letter
             correct_word_list.append(user_input)
             correct_word += user_input
-            #print("Right")
+                #print("Right")
+
         elif letter in correct_word: # we can use correct_word_list in the place of i and both are the same
             guesses += letter
+
         else:
             guesses += "_"
             #print("Wrong")
+
 
     if user_input not in word:
         lives -= 1
@@ -110,8 +118,12 @@ while game_over != False:
         #     print("You Loose!")
         #     game_over = False
     
+
     print(art_list.stages[lives])
     print(guesses)
+    if used_before == True:
+        print("You used this letter before !")
+
 
     if "_" not in guesses:
         print("********** You Win **********")
