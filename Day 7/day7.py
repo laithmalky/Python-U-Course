61a328af09c73fda10fa9e711578eec77231b431
Laith Malky <laith.malky@gmail.com>
2025-01-03 11:41:45 +0200

    commit

--- Day 7/day7.py
+++ Day 7/day7.py
@@ -1,6 +1,6 @@
 import random
 
-words_list = ["Apple", "Welcome", "Hello"]
+words_list = ["apple", "welcome", "hello"]
 
 # word = words_list[random.randint(0, len(words_list))]
 word = random.choice(words_list)
@@ -17,17 +17,19 @@ game_over = True
 correct_word = []
 i = ""
 
+lives = 6
+
 while game_over != False:
 
     guesses = ""
     user_input = input("Please write a letter: ").lower()
     for letter in word:
-        if user_input == letter.lower():
+        if user_input == letter:
             guesses += letter
             correct_word.append(user_input)
             i += user_input
             #print("Right")
-        elif letter in i:
+        elif letter in i: # we can use correct_word list in the place of i and both r the same
             guesses += letter
         else:
             guesses += "_"
@@ -37,3 +39,5 @@ while game_over != False:
     if "_" not in guesses:
         print("You Win!")
         game_over = False
+    else:
+        lives -= 1
