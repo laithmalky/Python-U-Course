24a7f2e930dbef0e2f727dfecd14634a87cfbd01
Laith Malky <laith.malky@gmail.com>
2025-01-06 22:41:27 +0200

    commit

--- Day 8/day8.py
+++ Day 8/day8.py
@@ -4,3 +4,11 @@ def greet():
     print("Good Night")
 
 greet()
+
+
+def greet_name(name):
+    print(f'Hello, {name}')
+    print(f'Good Morning {name}')
+    print(f'Good Night {name}')
+
+greet_name('John')
