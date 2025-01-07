f9f15cbb9ed26bfa2da6dd5a5296230b9bae4210
Laith Malky <laith.malky@gmail.com>
2025-01-07 21:04:41 +0200

    commit

--- Day 8/day8.py
+++ Day 8/day8.py
@@ -1,3 +1,4 @@
+# Description: Functions in Python 
 def greet():
     print("Hello")
     print("Good Morning")
@@ -6,9 +7,18 @@ def greet():
 greet()
 
 
+# Function with parameters and arguments
 def greet_name(name):
     print(f'Hello, {name}')
     print(f'Good Morning {name}')
     print(f'Good Night {name}')
 
 greet_name('John')
+
+# Function with more than 1 input
+def greet_with(name, location):
+    print(f'Hello, {name}')
+    print(f'Good Morning {name} from {location}')
+    print(f'Good Night {name} from {location}')
+
+greet_with('John', 'New York')
