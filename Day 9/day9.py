c65c716d2d8c78a08f3c1ab174e33948fe217d60
Laith Malky <laith.malky@gmail.com>
2025-02-05 22:12:19 +0200

    commit

---
+++ Day 9/day9.py
@@ -0,0 +1,10 @@
+first_dictionary = {"Bug": "An error in a program that prevents it from running as expected.",
+                    "Function": "A piece of code that you can easily call over and over again.",
+                    "Loop": "The action of doing something over and over again.",
+                    }
+
+# the dictionary is more like the list but it has key value pairs
+# the key is the word and the value is the definition of the word
+# you can access the value by using the key
+
+print(first_dictionary["Bug"])
