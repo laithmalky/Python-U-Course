0b05e4e671113eac074092dbc629a2e3d2b1ee62
Laith Malky <laith.malky@gmail.com>
2025-02-07 22:48:45 +0200

    testing

--- Day 9/day9-final-project.py
+++ Day 9/day9-final-project.py
@@ -5,6 +5,16 @@ print("Welcome to the secret auction program.")
 
 buyers = {}
 
+def find_buyer(bname, bmoney):
+    highest_money = 0
+    highest_buyer = ""
+    for word in buyers:
+        if buyers[word] > highest_money:
+            highest_money = buyers[word]
+            highest_buyer = word
+    print(f"The winner is {highest_buyer} with a payment of ${highest_money}")
+
+
 keep_going = True
 while keep_going:
     name = input("What is your name?: ")
@@ -16,9 +26,11 @@ while keep_going:
         if another_buyer.lower() == "no":
             keep_going = False
             uSure = False
+            find_buyer(name, money)
         elif another_buyer.lower() == "yes":
             uSure = False
             print("\n" * 100)
         else:
             uSure = True
+            print("\n" * 100)
             print("Please enter a valid input")
