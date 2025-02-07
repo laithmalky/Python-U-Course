5d36a601e3437090d11d05742c9add1373374bca
Laith Malky <laith.malky@gmail.com>
2025-02-07 21:45:59 +0200

    commit

--- Day 9/day9.py
+++ Day 9/day9.py
@@ -1,3 +1,4 @@
+
 first_dictionary = {"Bug": "An error in a program that prevents it from running as expected.",
                     "Function": "A piece of code that you can easily call over and over again.",
                     "Loop": "The action of doing something over and over again.",
