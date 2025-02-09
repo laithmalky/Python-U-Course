d9cb20daaf634a9b08de8e5888cb8a84f7ca2fb5
Laith Malky <laith.malky@gmail.com>
2025-02-09 20:50:10 +0200

    first task in day10

--- Day 10/day10.py
+++ Day 10/day10.py
@@ -1,1 +1,41 @@
-print('Hello')
+
+# Make the first letter of the first name and last name capital and the rest of the letters in lower case.
+
+# basic way
+def format_name(fname, lname):
+    f_name= ""
+    l_name = ""
+    for i in range(0, len(fname)):
+        if i == 0 :
+            f_name += fname[i].upper()
+        else:
+            f_name += fname[i].lower()
+
+    for i in range(0, len(lname)):
+        if i == 0 :
+            l_name += lname[i].upper()
+        else:
+            l_name += lname[i].lower()
+    
+    return f_name + " " + l_name
+            
+
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print(format_name(first_name, last_name))
+
+
+# efficient way
+
+def format_name(fname, lname):
+    # f_name = fname.capitalize()
+    # l_name = lname.capitalize()
+    # return f_name + " " + l_name
+    return fname.capitalize() + " " + lname.capitalize()
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print(format_name(first_name, last_name))
