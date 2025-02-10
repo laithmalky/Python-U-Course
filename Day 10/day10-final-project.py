bd46f9bfcf20306c8b4d5ee253e3745f69f786fc
Laith Malky <laith.malky@gmail.com>
2025-02-10 21:10:51 +0200

    day10 done

--- Day 10/day10-final-project.py
+++ Day 10/day10-final-project.py
@@ -1,6 +1,6 @@
 from art import logo
 
-print(logo)
+# print(logo) 
 
 def add(n1, n2):
     return n1 + n2
@@ -23,42 +23,46 @@ values = {
 
 # print(values["*"](4,8))
 
-first_input = float(input("Enter the first number: "))
+def calculator():
+    print(logo)
+    
+    first_input = float(input("Enter the first number: "))
 
-in_go = 'y'
-while in_go == 'y':
-    for key in values:
-        print(key)
-    
-    calculat = input("Pick an operation from the line above: ")
-
-    for word in range(0, 101):
-        if calculat in values:
-            break
-        else:
-            print("Invalid operation")
-            calculat = input("Please pick the right operation from the line above: ")
-
-    second_input = float(input("Enter the next number: "))
-    
-    calculation = values[calculat]
-    answer = calculation(first_input, second_input)
-    
-    print(f"{first_input} {calculat} {second_input} = {answer}")
-    
-    
-    for word in range(0,101):
+    in_go = 'y'
+    while in_go == 'y':
+        for key in values:
+            print(key)
         
-        in_go_check = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+        calculat = input("Pick an operation from the line above: ")
 
-        if in_go_check == 'y':
-            first_input = answer
-            break
+        for word in range(0, 101):
+            if calculat in values:
+                break
+            else:
+                print("Invalid operation")
+                calculat = input("Please pick the right operation from the line above: ")
 
-        elif in_go_check == 'n':
-            in_go = 'n'
-            print("Goodbye")
-            break
-    
-        else:
-            print("Invalid input")
+        second_input = float(input("Enter the next number: "))
+        
+        calculation = values[calculat]
+        answer = calculation(first_input, second_input)
+        
+        print(f"{first_input} {calculat} {second_input} = {answer}")
+        
+        
+        for word in range(0,101):
+
+            in_go_check = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+
+            if in_go_check == 'y':
+                first_input = answer
+                break
+
+            elif in_go_check == 'n':
+                print("\n" * 20)
+                calculator()
+
+            else:
+                print("Invalid input")
+
+calculator()
