ed5c9b9b8805bb91e5c6787fa66db29f62bc9d09
Laith Malky <laith.malky@gmail.com>
2025-02-10 20:34:38 +0200

    start testing

--- Day 10/day10-final-project.py
+++ Day 10/day10-final-project.py
@@ -21,5 +21,40 @@ values = {
     "/": divide,
     }
 
-print(values["*"](4,8))
+# print(values["*"](4,8))
 
+first_input = float(input("Enter the first number: "))
+
+in_go = 'y'
+while in_go == 'y':
+    for key in values:
+        print(key)
+    
+    calculat = input("Pick an operation from the line above: ")
+
+    for word in range(0, 101):
+        if calculat in values:
+            break
+        else:
+            print("Invalid operation")
+            calculat = input("Please pick the right operation from the line above: ")
+
+    second_input = float(input("Enter the next number: "))
+    
+    calculation = values[calculat]
+    answer = calculation(first_input, second_input)
+    
+    print(f"{first_input} {calculat} {second_input} = {answer}")
+    
+    in_go_check = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+    
+    if in_go_check == 'y':
+        first_input = answer
+
+    elif in_go_check == 'n':
+        in_go = 'n'
+        print("Goodbye")
+    
+    else:
+        print("Invalid input")
+        in_go_check = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
