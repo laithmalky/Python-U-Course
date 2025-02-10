e1ac9e32a12585e3f2ba98dbdf19e82b7ac6cc9d
Laith Malky <laith.malky@gmail.com>
2025-02-10 20:03:28 +0200

    building 1

--- Day 10/day10-final-project.py
+++ Day 10/day10-final-project.py
@@ -2,4 +2,24 @@ from art import logo
 
 print(logo)
 
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+values = { 
+    "+": add, 
+    "-": subtract, 
+    "*": multiply, 
+    "/": divide,
+    }
+
+print(values["*"](4,8))
 
