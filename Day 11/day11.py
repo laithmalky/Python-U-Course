8fe4df24f65af7463d1f57207e5f360eebb951a8
Laith Malky <laith.malky@gmail.com>
2025-02-26 19:18:17 +0200

    make the art

--- Day 11/day11.py
+++ Day 11/day11.py
@@ -1,2 +1,8 @@
+from day11_project_art import art
+
 
 game_start = input("Do you want to play a game of blackjack? Type 'y' for yes and 'n' for no: ")
+
+if game_start == 'y':
+    print(art)
+    print("Welcome to the game of Blackjack!")
