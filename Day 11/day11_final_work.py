f988e0dbd25cd8299b52a39257b1bd75b0b3e87e
Laith Malky <laith.malky@gmail.com>
2025-03-08 01:54:51 +0200

    editing

--- Day 11/day11_final_work.py
+++ Day 11/day11_final_work.py
@@ -1,3 +1,19 @@
 import random
 from day11_project_art import art
 
+def deal_cards():
+    # return random card from the list of cards
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+user_cards = []
+computer_cards = []
+
+for i in range(2):
+    # new_card = deal_cards()
+    # user_cards.append(new_card)
+    # computer_cards.append(new_card)
+    user_cards.append(deal_cards())
+    computer_cards.append(deal_cards())
+    
