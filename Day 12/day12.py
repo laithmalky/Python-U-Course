c35a7aa68d5667d3d988ec8734d5c3b7a0828ea3
Laith Malky <laith.malky@gmail.com>
2025-03-13 20:53:32 +0200

    start working

--- Day 12/day12.py
+++ Day 12/day12.py
@@ -7,3 +7,12 @@ def increase_enemies():
 increase_enemies()
 print(f"Enemies outside function: {enemies}")
 
+
+# Local scope: A variable created inside a function is available inside that function.
+
+def drink_potion():
+    potion_strength = 2
+    print(potion_strength)
+
+drink_potion()
+# print(potion_strength) # This will throw an error as potion_strength is not available outside the function.
