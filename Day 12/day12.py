b1fd66a148a471f692bd0e0c779556b41fe5b41c
Laith Malky <laith.malky@gmail.com>
2025-03-13 21:55:12 +0200

    namespace : local vs global in code

--- Day 12/day12.py
+++ Day 12/day12.py
@@ -21,11 +21,17 @@ drink_potion()
 # Global scope: A variable created outside a function is available inside the function.
 player_health = 10
 
-def drink_potion():
-    potion_strength = 2
-    print(player_health)
-    print(potion_strength)
 
-drink_potion()
+def game():
+    def drink_potion_T2():
+        potion_strength = 2
+        print(player_health)
+        print(potion_strength)
+    
+    drink_potion_T2()
+
+
+# drink_potion_T2() # This will throw an error as drink_potion_T2 is not available outside the function.
+game()
 print(player_health)
 # print(potion_strength) # This will throw an error as potion_strength is not available outside the function.
