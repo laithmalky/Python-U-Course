19234701061d3e28f142c42450d607e8086bded4
Laith Malky <laith.malky@gmail.com>
2025-03-13 21:35:51 +0200

    working

--- Day 12/day12.py
+++ Day 12/day12.py
@@ -16,3 +16,16 @@ def drink_potion():
 
 drink_potion()
 # print(potion_strength) # This will throw an error as potion_strength is not available outside the function.
+
+
+# Global scope: A variable created outside a function is available inside the function.
+player_health = 10
+
+def drink_potion():
+    potion_strength = 2
+    print(player_health)
+    print(potion_strength)
+
+drink_potion()
+print(player_health)
+# print(potion_strength) # This will throw an error as potion_strength is not available outside the function.
